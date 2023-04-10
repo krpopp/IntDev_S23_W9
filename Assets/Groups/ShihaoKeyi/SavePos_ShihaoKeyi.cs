567c1eeb4b9de96f60201158c1b77522148ab567
SomnusKL <117642820+SomnusKL@users.noreply.github.com>
2023-04-10 12:02:52 -0400

    add save position script

--- Assets/Groups/ShihaoKeyi/SavePos_ShihaoKeyi.cs
+++ Assets/Groups/ShihaoKeyi/SavePos_ShihaoKeyi.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPreps_ShihaoKeyi : MonoBehaviour
+public class SavePos_ShihaoKeyi : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("playerx"))
+        {
+            float x = PlayerPrefs.GetFloat("playerx");
+            float y = PlayerPrefs.GetFloat("playery");
+            transform.position = new Vector3(x, y, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerPrefs.SetFloat("playerx", transform.position.x);
+            PlayerPrefs.SetFloat("playery", transform.position.y);
+            Debug.Log("saved position");
+        }
     }
 }
