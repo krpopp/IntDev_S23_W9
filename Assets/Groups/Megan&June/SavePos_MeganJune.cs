4dfaaf42fbc6ed71644e2a02cbf5a4aab2a5ff09
June Shi <jjs833@nyu.edu>
2023-04-10 12:02:05 -0400

    implemented PlayerPrefs

--- Assets/Groups/Megan&June/SavePos_MeganJune.cs
+++ Assets/Groups/Megan&June/SavePos_MeganJune.cs
@@ -7,12 +7,22 @@ public class SavePos_MeganJune : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("playerX"))
+        {
+            float x = PlayerPrefs.GetFloat("playerX");
+            float y = PlayerPrefs.GetFloat("playerY");
+            transform.position = new Vector3(x, y, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerPrefs.SetFloat("playerX", transform.position.x);
+            PlayerPrefs.SetFloat("playerY", transform.position.y);
+            Debug.Log("saved position");
+        }
     }
 }
