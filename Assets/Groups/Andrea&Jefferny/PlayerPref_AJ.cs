97337e20fb667c5f76e4f3b7912f970a7d064d26
JeffernyWu <101135930+JeffernyWu@users.noreply.github.com>
2023-04-10 12:02:34 -0400

    [adding player prefs]

--- Assets/Groups/Andrea&Jefferny/PlayerPref_AJ.cs
+++ Assets/Groups/Andrea&Jefferny/PlayerPref_AJ.cs
@@ -7,12 +7,26 @@ public class PlayerPref_AJ : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // check to see if you have a saved position and then go if you do 
+        if (PlayerPrefs.HasKey("playerX"))
+        {
+            // get the save position 
+            float x = PlayerPrefs.GetFloat("playerX");
+            float y = PlayerPrefs.GetFloat("playerY");
+            // set player as position 
+            transform.position = new Vector3(x, y, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // playerprefs store strings, floats, & int
+            PlayerPrefs.SetFloat("playerX", transform.position.x); //1st argument is the key
+            PlayerPrefs.SetFloat("playerY", transform.position.y); //2nd argument is player position 
+            Debug.Log("saved position");
+        }
     }
 }
